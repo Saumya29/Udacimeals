8d596bd81386370025a34782df385b6524695ea2
saumya29 <saumyatiwari.29@gmail.com>
2018-07-02 22:27:47 +0530

    :books:

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    calender: null
+  }
+  componentDidMount(){
+    const { store } = this.props;
+    store.subscribe(() => {
+      this.setState(() => {
+        calender: store.getState();
+      })
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <input
+        tyoe = 'text'
+        ref = {(input) => this.input = input}
+        placeholder = 'Monday breakfast'
+        />
+        <button onClick={this.submitFood}>Submit</button>
+
+        <pre>
+          Monday's Breakfast: {this.state.calender && this.state.calender.monday.breakfast}
+        </pre>
       </div>
     );
   }
